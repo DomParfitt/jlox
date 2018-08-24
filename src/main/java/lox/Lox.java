49cc750efb8548f06c04db63743b10885bad46f4
Dom Parfitt <dom.parfitt@gmail.com>
2018-08-24 13:04:47 +0100

    Interpreter implementation

--- src/main/java/lox/Lox.java
+++ src/main/java/lox/Lox.java
@@ -10,7 +10,10 @@ import java.util.List;
 
 public class Lox {
 
+    private static final Interpreter interpreter = new Interpreter();
+
     private static boolean hadError = false;
+    private static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -29,6 +32,10 @@ public class Lox {
 
         if (hadError) {
             System.exit(65);
+        }
+
+        if (hadRuntimeError) {
+            System.exit(70);
         }
     }
 
@@ -53,7 +60,7 @@ public class Lox {
             return;
         }
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     //Error handling
@@ -67,6 +74,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    public static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
---
+++ src/main/java/lox/RuntimeError.java
@@ -0,0 +1,11 @@
+package lox;
+
+public class RuntimeError extends RuntimeException {
+
+    public final Token token;
+
+    public RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
+}
